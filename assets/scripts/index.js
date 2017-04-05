820608bbfa397049a8f58c34985e13d2a2dc8de7
deeforte <deirdre.forte@libertymutual.com>
2017-04-05 13:21:02 -0400

    First commit (after initial) for tic tac toe game project
    Added: game board, logic to win, placing X or O on board,
    console logs to show who won and where the 3-match is.
    Need to add reset, signup/in/out & chg pswd functionality

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,57 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+console.log('up and runnning')
+
+const squaresInPlay = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+let numClicks = 0
+let numGames = 0
+
+// put on click function here
+
+// add check for win here
+const checkForWin = function () {
+  console.log('I made it to checkForWin function', squaresInPlay)
+  if ((squaresInPlay[0] === squaresInPlay[1]) && (squaresInPlay[1] === squaresInPlay[2])) {
+    console.log(squaresInPlay[0], 'WINS! - top')
+  } else if ((squaresInPlay[0] === squaresInPlay[3]) && (squaresInPlay[3] === squaresInPlay[6])) {
+    console.log(squaresInPlay[0], 'WINS! - left')
+  } else if ((squaresInPlay[1] === squaresInPlay[4]) && (squaresInPlay[4] === squaresInPlay[7])) {
+    console.log(squaresInPlay[1], 'WINS! - centerV')
+  } else if ((squaresInPlay[2] === squaresInPlay[5]) && (squaresInPlay[5] === squaresInPlay[8])) {
+    console.log(squaresInPlay[2], 'WINS! - right')
+  } else if ((squaresInPlay[6] === squaresInPlay[7]) && (squaresInPlay[7] === squaresInPlay[8])) {
+    console.log(squaresInPlay[6], 'WINS! - bottom')
+  } else if ((squaresInPlay[3] === squaresInPlay[4]) && (squaresInPlay[4] === squaresInPlay[5])) {
+    console.log(squaresInPlay[3], 'WINS! - centerH')
+  } else if ((squaresInPlay[0] === squaresInPlay[4]) && (squaresInPlay[4] === squaresInPlay[8])) {
+    console.log(squaresInPlay[0], 'WINS! - diagLtoR')
+  } else if ((squaresInPlay[2] === squaresInPlay[4]) && (squaresInPlay[4] === squaresInPlay[6])) {
+    console.log(squaresInPlay[2], 'WINS! - diagRtoL')
+  }
+  numGames = numGames + 1
+}
+
+const fillSqInPlay = function () {
+  const gridNum = this.id
+  numClicks = numClicks + 1
+  console.log('clicked on a square', gridNum, numClicks)
+  if (numClicks % 2 === 0) {
+    squaresInPlay[gridNum] = 'O'
+    this.setAttribute('src', 'assets/images/blueO.png')
+  } else {
+    squaresInPlay[gridNum] = 'X'
+    this.setAttribute('src', 'assets/images/redX.png')
+  }
+  if (numClicks > 4) {
+    checkForWin(squaresInPlay)
+  }
+}
 
 $(() => {
   setAPIOrigin(location, config)
+  $('.gameBoard').children('').children('').children('').on('click', fillSqInPlay)
 })
 
 // use require with a reference to bundle the file and use it in this file
